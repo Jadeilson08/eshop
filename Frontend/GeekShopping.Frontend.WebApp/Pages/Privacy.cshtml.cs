bd979806a9268da0e83df0579e02fbc7e0fa38c0
jadeilson <jadeilson08@gmail.com>
2022-08-16 19:48:40 -0400

    create base project

---
+++ Frontend/GeekShopping.Frontend.WebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeekShopping.Frontend.WebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
